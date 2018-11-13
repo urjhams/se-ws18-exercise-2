d9d221e03482d5c2c25b36ff46106ac4b25d72fa
Quan <urjhams@gmail.com>
2018-11-13 23:48:44 +0100

    finished

--- Plane.java
+++ Plane.java
@@ -13,16 +13,16 @@ public class Plane extends Entity {
 
     Vec3D intersect(Vec3D ray) {
         float ds = ray.len();									// distance
-        Vec3D l = new Vec3D(ray.x / ds, ray.y / ds, ray.z / ds);// unit vector  - direction of the line
+        Vec3D l = new Vec3D(ray.x / ds, ray.y / ds, ray.z / ds);// unit vector - direction of the line
 
         // Algebraic form: https://en.wikipedia.org/wiki/Line–plane_intersection#Parametric_form
-        // d = (p0 - l0) * n / l * n
-        float top = point.sub(ray).mul(this.normalVector);      // (p0 - l0) * n    -- lo: ray, p0: point(pointOnPlane)
+        // d = (p0 - l0) * n / (l * n)
+        float top = point.sub(ray).mul(this.normalVector);      // (p0 - l0) * n -- lo: ray, p0: point(pointOnPlane)
         float bottom = l.mul(this.normalVector);                // l * n
         
         if (bottom != 0) {                                      // intersect in a single point
-            float d = top / bottom;
-            return l.mul(d).sub(ray);                           // the point of intersection is given by d * l + l0
+            float d = top / bottom;                             // d is the real scalar in number domain
+            return l.mul(d).add(ray);                           // the point of intersection is given by d * l + l0
         } else {                                                // parallel
             if (top == 0) {                                     // the vector is contained on the plane
                 return null;
